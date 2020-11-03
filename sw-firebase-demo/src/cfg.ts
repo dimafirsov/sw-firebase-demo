bc24866cc7307cdc6e9209b3b174e5d67735e6e2
Dmytro Firsov <dmitriy.firsov@solarwinds.com>
2020-11-03 15:55:26 +0200

    new project working

--- sw-firebase-demo/src/cfg.ts
+++ sw-firebase-demo/src/cfg.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: 'AIzaSyAEvQ6OWhUo1cMonKu1Sj_076qP03A5VGo',
-    authDomain: 'sw-demo-b79fa.firebaseapp.com',
-    databaseURL: 'https://sw-demo-b79fa.firebaseio.com',
-    projectId: 'sw-demo-b79fa',
-    storageBucket: 'sw-demo-b79fa.appspot.com',
-    messagingSenderId: '395883247774',
-    appId: '1:395883247774:web:8850968dc1ddd7e792a38f'
+    apiKey: 'AIzaSyDwn_Rbvd2VgLCg2GAz-SS9bufQ9r3-4pE',
+    authDomain: 'sw-demo-f7f8c.firebaseapp.com',
+    databaseURL: 'https://sw-demo-f7f8c.firebaseio.com',
+    projectId: 'sw-demo-f7f8c',
+    storageBucket: 'sw-demo-f7f8c.appspot.com',
+    messagingSenderId: '767396037609',
+    appId: '1:767396037609:web:8708628f7fe161469d8178'
 };
